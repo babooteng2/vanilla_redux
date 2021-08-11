a461b2df77cd1ac6f9326dae543bb48ffe3e66fc
Tom Kim <babooteng2@gmail.com>
2021-08-11 23:14:08 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import { createStore } from "redux";
 // create .env.local file and add in REACT_EDITOR=code
 let count = 0;
 
-const reduceModifier = (state = 0) => {
-  return state;
+const reduceModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "PLUS") {
+    count++;
+  } else if (action.type === "MINUS") {
+    count--;
+  }
+  return count;
 };
 const countStore = createStore(reduceModifier);
 
@@ -12,3 +18,11 @@ const countStore = createStore(reduceModifier);
   createStore는 reducer를 인자로 받고 reducer는 유일하게 state를 변경할 수 있는 유일한 함수
   state를 변경할 땐 action을 통해 가능
   */
+
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "MINUS" });
+console.log(countStore.getState());
