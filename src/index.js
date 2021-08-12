eb782908f2c8fff908fa66d2d8c2613b9c58700c
Tom Kim <babooteng2@gmail.com>
2021-08-13 01:13:09 +0900

    #2.2 Delete To Do - split actions

--- src/index.js
+++ src/index.js
@@ -5,26 +5,77 @@ const inputTodo = toDoList.querySelector(".inputTodo");
 const submitBtn = toDoList.querySelector(".submitBtn");
 const ul = toDoList.querySelector(".list");
 
-/* Vanilla javascript
-const paint = (toDo) => {
-  const addList = document.createElement("li");
-  addList.innerText = toDo;
-  ul.appendChild(addList);
-};
-const hd_submit = (e) => {
-  e.preventDefault();
-  console.log(`value: ${inputTodo.value}`);
-  if (inputTodo.value !== "") {
-    const toDo = inputTodo.value;
-    inputTodo.value = "";
-    paint(toDo);
-  }
-}; */
+const ADD = "add";
+const DELETE = "delete";
 
-/* state는 single source of truth이며, read-only이다
-store을 수정할 수 있는 유일한 방법은 action을 보내는 방법뿐이다.
-state를 mutate하지 말아야한다.
-mutating state하는 대신에 new state objects를 리턴해야 한다. */
+/* actions - onloy for dispatch (usally position onto reducer)
+  최대한 작은 단위로 쪼갬
+*/
+const addToDo = (text) => {
+  const id = Date.now();
+  return {
+    type: ADD,
+    text,
+    id,
+  };
+};
+
+const deleteToDo = (id) => {
+  //const id = e.target.parentNode.id;
+  //store.dispatch({ type: DELETE, id });
+  return {
+    type: DELETE,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: action.id }, ...state];
+    case DELETE:
+      return [{ id: action.id }, ...state];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const { text, id } = toDo;
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    btn.innerText = "x";
+    li.id = id;
+    li.innerText = text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+const dispactchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+
+store.subscribe(() => {
+  /*   console.log(store.getState());
+  const addList = document.createElement("li");
+  const { text } = store.getState();
+  addList.innerText = text;
+  ul.appendChild(addList); */
+  paintToDos();
+});
 
 const hd_submit = (e) => {
   e.preventDefault();
@@ -33,25 +84,9 @@ const hd_submit = (e) => {
     const toDo = inputTodo.value;
     inputTodo.value = "";
     //paint(toDo);
-    store.dispatch({ type: ADD, text: toDo });
+    //store.dispatch({ type: ADD, text: toDo });
+    dispactchAddToDo(toDo);
   }
 };
-const ADD = "add";
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return { text: action.text };
-    default:
-      return state;
-  }
-};
-const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
-  const addList = document.createElement("li");
-  const { text } = store.getState();
-  addList.innerText = text;
-  ul.appendChild(addList);
-});
 
 submitBtn.addEventListener("click", hd_submit);
