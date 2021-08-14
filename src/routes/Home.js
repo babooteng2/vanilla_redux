a1556a6f56af20c3c147985faf23fbcaca75d76b
Tom Kim <babooteng2@gmail.com>
2021-08-14 14:03:34 +0900

    #3.2 mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -17,9 +18,24 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+// https://react-redux.js.org/using-react-redux/connect-mapstate
+/* 
+  1. state - redux store에서 온 state
+  2. ownProps - router에서 온 prop 
+    - 이 ownProps는 export default connect(getCurrentStaet)(Home);
+      이 라인에서 Home에 intercept하여 prop을 추가시켜 줌
+    - 결론: react router에서 준 props 말고도 ( but also ) ownProps를 추가시켜 줌
+*/
+/* function mapStateToProps(state, ownProps) {
+  return { state, sexy: true };  
+} */
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
